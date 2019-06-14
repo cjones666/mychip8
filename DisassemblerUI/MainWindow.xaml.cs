823d12702105c3f015d250e457ac0fa5dff3aa2b
Chris Jones <chrisjamesjones@gmail.com>
2019-06-13 20:24:20 -0500

    Adds initial Disassembler UI project.

---
+++ DisassemblerUI/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace DisassemblerUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFileButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FileTextBox.Text))
+            {
+                // Show the file picker
+                var fileDialog = new OpenFileDialog {CheckFileExists = true, CheckPathExists = true};
+                if (fileDialog.ShowDialog() == true)
+                {
+                    // Load the file
+                }
+            }
+            else
+            {
+                if (File.Exists(FileTextBox.Text))
+                {
+                    // Do a check here with the disassemble to determine that this is a valid chip 8 file                       
+                }
+                else
+                {   
+                    // Show the error message box and clear the box
+                    MessageBox.Show("File does not exit");
+                    FileTextBox.Text = string.Empty;
+                }
+
+            }
+        }
+
+        private void ShowHexCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // Update the instruction data to convert all hexadecimal to decimal or vice versa.
+            MessageBox.Show("Checkbox");
+        }
+    }
+}
--- MyChip8Disassembler/Disassembler/Disassembler.cs
+++ MyChip8Disassembler/Disassembler/Disassembler.cs
@@ -10,11 +10,11 @@ namespace MyChip8Disassembler.Disassembler
 
         public Disassembler()
         {
-            var fileBytes = ProgramLoader.Load("D:\\Dev\\Chip8\\CHIP8\\GAMES\\PONG");
+            var fileBytes = ProgramLoader.Load("C:\\Dev\\mychip8\\MyChip8\\GAMES\\PONG");
             _chip8 = new Chip8System();
             _chip8.LoadProgram(fileBytes);
 
-            for (var i = 0x0200; i < _chip8.Memory.TotalMemory; i = i + 2)
+            for (var i = 0x0200; i < _chip8.Memory.TotalMemory; i += 2)
             {
                 var currentByte = _chip8.Memory.ReadByteAtAddress(i);
                 var nextByte = _chip8.Memory.ReadByteAtAddress(i + 1);
